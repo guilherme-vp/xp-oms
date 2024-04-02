5a161c32a4be641e05a0fd20d258fe621f986f0b
Guilherme Vieira de Paiva <guivpw68@gmail.com>
2024-04-01 21:48:05 -0300

    feat: update concurrency, max instances and min instances

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,18 @@ import { onRequest } from 'firebase-functions/v2/https'
 import 'reflect-metadata'
 
 export const web = onRequest(
-  { secrets: [mongoURLKey], region: 'southamerica-east1' },
-  (req, res) => {
+  {
+    secrets: [mongoURLKey],
+    region: 'southamerica-east1',
+    concurrency: 1,
+    maxInstances: 10
+  },
+  async (req, res) => {
     logger.info('Incoming Request', req)
+    const promise = new Promise(resolve => {
+      setTimeout(() => resolve(true), 1000)
+    })
+    await promise
     import('./entrypoints/web').then(({ default: app }) => app(req, res))
   }
 )
